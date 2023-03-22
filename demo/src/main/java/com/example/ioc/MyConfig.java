4ab48a3920754cd8a0f7305daceeac3cb58d9304
clopezri <clopezri@host.docker.internal>
2023-03-22 11:15:33 +0100

    pom

--- demo/src/main/java/com/example/ioc/MyConfig.java
+++ demo/src/main/java/com/example/ioc/MyConfig.java
@@ -2,7 +2,7 @@ package com.example.ioc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
