9507ee88cee4b207b79257aa45be36665f173e39
clopezri <clopezri@host.docker.internal>
2023-03-28 13:11:27 +0200

    FindBy

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import com.example.domains.contract.repositories.ActorRepository;
 import com.example.domains.entities.Actor;
@@ -26,16 +27,22 @@ public class DemoApplication implements CommandLineRunner {
 //		(new EjemplosIoC()).run();
 //		var actors = new Actor(0, "Pepito", "Grillo");
 //		dao.save(actors);
-		dao.deleteById(205);
-		var item = dao.findById(204);
-		if(item.isPresent()) {
-			var actor = item.get();
-			actor.setLastName(actor.getLastName().toUpperCase());
-			actor.setFirstName(actor.getFirstName().toUpperCase());
-			dao.save(actor);
-			dao.findAll().forEach(System.out::println);
-		} else {
-			System.out.println("Actor no encontrado");
-		}
+//		dao.deleteById(205);
+//		var item = dao.findById(204);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setLastName(actor.getLastName().toUpperCase());
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//			dao.findAll().forEach(System.out::println);
+//		} else {
+//			System.out.println("Actor no encontrado");
+//		}
+//		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("p")
+//			.forEach(System.out::println);
+		dao.findTop5ByFirstNameStartingWith("p", Sort.by("LastName").descending())
+			.forEach(System.out::println);
+		dao.findTop5ByFirstNameStartingWith("p", Sort.by("LastName"))
+		.forEach(System.out::println);
 	}
 }
