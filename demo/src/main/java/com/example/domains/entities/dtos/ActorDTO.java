e6d5bd4786a6f9f69c55db2c15744b78ed454ca8
clopezri <clopezri@host.docker.internal>
2023-03-29 13:13:45 +0200

    DTO 2

--- demo/src/main/java/com/example/domains/entities/dtos/ActorDTO.java
+++ demo/src/main/java/com/example/domains/entities/dtos/ActorDTO.java
@@ -8,16 +8,16 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Value;
 
-//@Value
-@Data @AllArgsConstructor @NoArgsConstructor
+@Value
+//@Data @AllArgsConstructor @NoArgsConstructor
 public class ActorDTO {
 	private int actorId;
 	private String firstName;
 	private String lastName;
 	
-//	public static ActorDTO from(/* @NonNull */ Actor target) {
-//		return new ActorDTO(target.getActorId(), target.getFirstName(), target.getLastName());
-//	}
+	public static ActorDTO from(/* @NonNull */ Actor target) {
+		return new ActorDTO(target.getActorId(), target.getFirstName(), target.getLastName());
+	}
 
 	public static Actor from(ActorDTO target) {
 		return new Actor(target.getActorId(), target.getFirstName(), target.getLastName());
