5dd69b53e083edab623a9cfc78376e9f099f4025
clopezri <clopezri@host.docker.internal>
2023-03-30 08:51:37 +0200

    Catalogo project

---
+++ catalogo/src/main/java/com/example/domains/entities/Film.java
@@ -0,0 +1,167 @@
+package com.example.domains.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+/**
+ * The persistent class for the film database table.
+ * 
+ */
+@Entity
+@Table(name="film")
+@NamedQuery(name="Film.findAll", query="SELECT f FROM Film f")
+public class Film implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="film_id", unique=true, nullable=false)
+	private int filmId;
+
+	@Lob
+	private String description;
+
+	@Column(name="last_update", insertable=false, updatable=false, nullable=false)
+	private Timestamp lastUpdate;
+
+	private int length;
+
+	@Column(name="release_year")
+	private Short releaseYear;
+
+	@Column(nullable=false, length=128)
+	private String title;
+
+	//bi-directional many-to-one association to Language
+	@ManyToOne
+	@JoinColumn(name="language_id", nullable=false)
+	private Language language;
+
+	//bi-directional many-to-one association to Language
+	@ManyToOne
+	@JoinColumn(name="original_language_id")
+	private Language languageVO;
+
+	//bi-directional many-to-one association to FilmActor
+	@OneToMany(mappedBy="film")
+	private List<FilmActor> filmActors;
+
+	//bi-directional many-to-one association to FilmCategory
+	@OneToMany(mappedBy="film")
+	private List<FilmCategory> filmCategories;
+
+	public Film() {
+	}
+
+	public int getFilmId() {
+		return this.filmId;
+	}
+
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Timestamp getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public Short getReleaseYear() {
+		return this.releaseYear;
+	}
+
+	public void setReleaseYear(Short releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Language getLanguage() {
+		return this.language;
+	}
+
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
+
+	public Language getLanguageVO() {
+		return this.languageVO;
+	}
+
+	public void setLanguageVO(Language languageVO) {
+		this.languageVO = languageVO;
+	}
+
+	public List<FilmActor> getFilmActors() {
+		return this.filmActors;
+	}
+
+	public void setFilmActors(List<FilmActor> filmActors) {
+		this.filmActors = filmActors;
+	}
+
+	public FilmActor addFilmActor(FilmActor filmActor) {
+		getFilmActors().add(filmActor);
+		filmActor.setFilm(this);
+
+		return filmActor;
+	}
+
+	public FilmActor removeFilmActor(FilmActor filmActor) {
+		getFilmActors().remove(filmActor);
+		filmActor.setFilm(null);
+
+		return filmActor;
+	}
+
+	public List<FilmCategory> getFilmCategories() {
+		return this.filmCategories;
+	}
+
+	public void setFilmCategories(List<FilmCategory> filmCategories) {
+		this.filmCategories = filmCategories;
+	}
+
+	public FilmCategory addFilmCategory(FilmCategory filmCategory) {
+		getFilmCategories().add(filmCategory);
+		filmCategory.setFilm(this);
+
+		return filmCategory;
+	}
+
+	public FilmCategory removeFilmCategory(FilmCategory filmCategory) {
+		getFilmCategories().remove(filmCategory);
+		filmCategory.setFilm(null);
+
+		return filmCategory;
+	}
+
+}
