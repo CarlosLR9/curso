ae4918a5cceebba39969b9e797bc1138110425f6
clopezri <clopezri@host.docker.internal>
2023-04-03 12:42:27 +0200

    Job

--- lotes/src/main/java/com/example/batch/PersonaItemProcessor.java
+++ lotes/src/main/java/com/example/batch/PersonaItemProcessor.java
@@ -10,13 +10,12 @@ import com.example.model.PersonaDTO;
 @Component
 public class PersonaItemProcessor implements ItemProcessor<PersonaDTO, Persona> {
 	private static final Logger log = LoggerFactory.getLogger(PersonaItemProcessor.class);
-
+	
 	@Override
-	public Persona process(PersonaDTO item) throws Exception {
-		if (item.getId() % 2 == 0 || "Male".equals(item.getSexo()))
-			return null;
-		Persona rslt = new Persona(item.getId(), item.getApellidos() + ", " + item.getNombre(), item.getCorreo(),
-				item.getIp());
+	public Persona process(PersonaDTO item) throws Exception {	 
+		if(item.getId() % 2 == 0 || "Male".equals(item.getSexo())) return null;
+		Persona rslt = new Persona(item.getId(), item.getApellidos() + ", " + item.getNombre(), 
+				item.getCorreo(), item.getIp());
 		log.info("Procesando: " + item);
 		return rslt;
 	}
