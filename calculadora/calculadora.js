27683b973feaae42f194665e41dd77350dedda06
CarlosLR9 <carlos.lopez-riba@capgemini.com>
2023-04-18 23:03:20 +0200

    Calc

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,130 +1,96 @@
 function calculadora(){
     let result = document.querySelector('#rslt')
     let op = document.querySelector('#op')
-    let op1 = document.querySelector('#op1')
-    let op2 = document.querySelector('#op2')
-    
-    let btn1 = document.querySelector('#id1')
-    let btn2 = document.querySelector('#id2')
-    let btn3 = document.querySelector('#id3')
-    let btn4 = document.querySelector('#id4')
-    let btn5 = document.querySelector('#id5')
-    let btn6 = document.querySelector('#id6')
-    let btn7 = document.querySelector('#id7')
-    let btn8 = document.querySelector('#id8')
-    let btn9 = document.querySelector('#id9')
-    let btn0 = document.querySelector('#id0')
-    
-    let btnSum = document.querySelector('#sum')
-    let btnRes = document.querySelector('#res')
-    let btnMult = document.querySelector('#mult')
-    let btnDiv = document.querySelector('#div')
-    let btnIgual = document.querySelector('#igual')
-    
-    // op1.hidden = !op1.hidden
-    op2.hidden = !op2.hidden
-    op.hidden = !op.hidden
-    result.hidden = !result.hidden
-    document.querySelector('#span').hidden = !document.querySelector('#span').hidden
+    let btnRe = document.querySelector('#re')
+    let btnBorrar = document.querySelector('#borrar')
+    let btnDec = document.querySelector('#dec')
+    let btnNums = document.querySelectorAll('#num')
+    let btnCalcs = document.querySelectorAll('#calc')
 
-    let operador
-    let n1 = 0
-    let n2 = 0
-    let n = 0
-    let n1fin = 1
-    let n2fin = 1
+    let operador = '+'
+    let n1 = '0'
+    let n = '0'
+    let nfin = true
+    let opFin = true
     let solucion = 0
+    let opEntera = false
+    result.textContent = n
+    op.textContent = ''
 
-    op1.textContent = n
+    function operacion(value){
 
-    function operacion(){
-        op1.textContent = n1
-        solucion = eval(solucion + n1 + operador + +n)
-        result.hidden = false
-        document.querySelector('#span').hidden = false
-        result.textContent = solucion
-        op.textContent = operador
-        op2.textContent = n
-        n = 0
-        n1fin = 1
+        switch (operador) {
+			case '+':
+				solucion += +n;
+				break;
+			case '-':
+				solucion -= +n;
+				break;
+			case '*':
+				solucion *= +n;
+				break;
+			case '/':
+				solucion /= +n;
+				break;
+		}
+
+        op.textContent = value == '=' ? '' : (`${solucion} ${value}`);
+        n = parseFloat(solucion.toPrecision(15)).toString();
+        result.textContent = n
+        nfin = false
+        operador = value
+        opEntera = false
     }
 
-    btnIgual.addEventListener('click', ev => {
-        operacion()
-    })
-
-    btnSum.addEventListener('click', ev => {
+    function reiniciar()
+    {
         operador = '+'
-        op.textContent = operador
-        op.hidden = false
-        if (!n1fin) {
-            operacion()
-        }
-        else{
-            n1 = n
-            n1fin = 0
-        }
-        n = 0
-    })
-    btnRes.addEventListener('click', ev => {
-        operador = '-'
-        operacion()
-    })
-    btnMult.addEventListener('click', ev => {
-        operador = '*'
-        operacion()
-    })
-    btnDiv.addEventListener('click', ev => {
-        operador = '/'
-        operacion()
-    })
+        n1 = '0'
+        n = '0'
+        nfin = true
+        opFin = true
+        solucion = 0
+        opEntera = false
+        result.textContent = n
+        op.textContent = ''
+    }
 
-    btn1.addEventListener('click', ev => {
-        n = eval(n + '1')
-        if (n1fin) {
-            op1.textContent = n
-        } 
-        else {
-            op2.textContent = n
-            op2.hidden = false
-        }
-        
-    })
-    btn2.addEventListener('click', ev => {
-        n = n + 2
-        op2.textContent = n
-    })
-    btn3.addEventListener('click', ev => {
-        n = n + 3
-        op2.textContent = n
-    })
-    btn4.addEventListener('click', ev => {
-        n = n + 4
-        op2.textContent = n
-    })
-    btn5.addEventListener('click', ev => {
-        n = n + 5
-        op2.textContent = n
-    })
-    btn6.addEventListener('click', ev => {
-        n = n + 6
-        op2.textContent = n
-    })
-    btn7.addEventListener('click', ev => {
-        n = n + 7
-        op2.textContent = n
-    })
-    btn8.addEventListener('click', ev => {
-        n = n + 8
-        op2.textContent = n
-    })
-    btn9.addEventListener('click', ev => {
-        n = n + 9
-        op2.textContent = n
-    })
-    btn0.addEventListener('click', ev => {
-        n = n + 0
-        op2.textContent = n
-    })
+    function borrar(){
+        if (!nfin || n.length == 1 || (n.length == 2 && n.startsWith('-'))) {
+			n = '0';
+			nfin = true;
+		} else
+			n = n.slice(0, -1);
+        result.textContent = n
+    }
 
+    function decimal() {
+		if (!nfin) {
+			n = '0.';
+			nfin = true;
+		} else if (n.indexOf('.') === -1)
+			n += '.';
+        result.textContent = n
+	};
+
+    btnCalcs.forEach(btn => {
+        btn.addEventListener('click', ev => {
+            operacion(ev.target.value)
+    })})
+    
+    btnNums.forEach(btn => {
+        btn.addEventListener('click', ev => {
+            if(nfin && n !=0 )
+                n += ev.target.value
+            else
+            {
+                n = ev.target.value
+                nfin = true
+            }
+            result.textContent = n
+    })})
+
+    btnRe.addEventListener('click', reiniciar)
+    btnBorrar.addEventListener('click', borrar)
+    btnDec.addEventListener('click', decimal)
 }
--- demo-react/src/App.js
+++ demo-react/src/App.js
@@ -1,11 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { Component } from 'react'
+import Contador from './componentes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <main>
+         <Contador init={10} delta={2} />
+        </main>
+        {/* <Home />
+        <DemosJSX /> */}
+      </>
+    )
+  }
+}
+
+class DemosJSX extends Component {
+  render() { 
+    let nombre = 'mundo'
+    let estilo = 'App-link'
+    let saluda = <h1>Hola {nombre.toUpperCase() +'!'}!!</h1>
+    let dim = {width: 100, hegiht: 50}
+    let errorStyle = {color: 'white', backgroundColor: 'red'}
+    return (
+      <>
+        {saluda}
+        <div style={errorStyle}>DemosJSX</div>
+        <h2 className={estilo}>Hola {nombre}</h2>
+        <img src={logo} className="App-logo" alt="logo" {...dim} hidden={false} />
+      </>
+    )
+  }
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hola mundo</h1>
+        <h2>url: {process.env.API_URL}</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +59,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
